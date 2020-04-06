0e169560dc08a25031b3de9c4eabca32d8bfd1bf
Julien Rousseau <julrousseau20@gmail.com>
2020-04-06 15:21:49 -0400

    Gestion changement grosseur verticale

--- site/js/index/index_height_format.js
+++ site/js/index/index_height_format.js
@@ -2,10 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     SetIndexHeight();
 });
 
+window.addEventListener("resize", () => {
+    SetIndexHeight();
+});
+
 document.body.addEventListener("scroll", () => {
     SetIndexHeight();
 });
 
 function SetIndexHeight() {
-    document.getElementById("main_page").style.height = window.innerHeight + "px";
+    height = window.innerHeight;
+    if (height < 600)
+        height = 600;
+    document.getElementById("main_page").style.height = height + "px";
 }
