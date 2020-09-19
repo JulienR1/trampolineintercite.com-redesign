d5d3e94d408f9c4580b51ce1f0b1258576418f7d
Julien <julrousseau20@gmail.com>
2020-09-19 11:58:24 -0400

    Styling setup

--- site/includes/views/header.php
+++ site/includes/views/header.php
@@ -4,47 +4,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title><?php echo static::$info->title; ?></title>
+
+    <link rel="stylesheet" href="css/general/general.css?v=1">
+    <link rel="stylesheet" href="css/header/header.css?v=1">
+    <link rel="stylesheet" href="css/footer/footer.css?v=1">
 </head>
 
 <body>
 
-<header>
-    <div class="bg-shadow desktop-hide-scroll">
-        <div class="desktop-hide">
-            <div id="logo"><img src="/assets/img/logo.png" alt="Logo"></div>
-            <div id="nav-button"><!-- style with ::before and ::after --></div>
-        </div>
+    <header>
+        <div class="bg-shadow desktop-hide-scroll">
+            <div class="desktop-hide">
+                <div id="logo"><img src="/assets/img/logo.png" alt="Logo"></div>
+                <div id="nav-button">
+                    <!-- style with ::before and ::after -->
+                </div>
+            </div>
 
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">À propos</a>
-                    <ul>
-                        <li><a href="#">Actualités</a></li>
-                        <li><a href="#">Règlements</a></li>
-                        <li><a href="#">Annonces</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Activités</a>
-                    <ul>
-                        <li><a href="#">Récréatif</a></li>
-                        <li><a href="#">Fête d'enfants</a></li>
-                        <li><a href="#">Compétitif</a></li>
-                        <li><a href="#">Comment s'inscrire</a></li>
-                    </ul>
-                </li>
-                <li class="desktop-only">
-                    <a href="#"><img src="/assets/img/logo.png" alt="Logo"></a>
-                </li>
-                <li>
-                    <a href="#">Horaire</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-</header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">À propos</a>
+                        <ul>
+                            <li><a href="#">Actualités</a></li>
+                            <li><a href="#">Règlements</a></li>
+                            <li><a href="#">Annonces</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Activités</a>
+                        <ul>
+                            <li><a href="#">Récréatif</a></li>
+                            <li><a href="#">Fête d'enfants</a></li>
+                            <li><a href="#">Compétitif</a></li>
+                            <li><a href="#">Comment s'inscrire</a></li>
+                        </ul>
+                    </li>
+                    <li class="desktop-only">
+                        <a href="#"><img src="/assets/img/logo.png" alt="Logo"></a>
+                    </li>
+                    <li>
+                        <a href="#">Horaire</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
--- site/routes.php
+++ site/routes.php
@@ -4,11 +4,6 @@ Route::set("index.php", function () {
     Home::CreateView("home");
 });
 
-Route::set("404", function () {
-    PageNotFound::CreateView("pageNotFound");
-});
-
 if (!in_array($_GET["url"], Route::$validRoutes)) {
-    header("Location: /404");
-    exit();
+    PageNotFound::CreateView("pageNotFound");
 }
