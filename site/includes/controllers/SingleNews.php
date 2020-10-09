c9d7d77b0a0b2b6ec6d5454109eb24b4ebe99d19
Julien <julrousseau20@gmail.com>
2020-10-08 23:47:03 -0400

    Style page individuelle actualites

--- site/includes/controllers/SingleNews.php
+++ site/includes/controllers/SingleNews.php
@@ -16,12 +16,12 @@ class SingleNews extends Controller
     {
         parent::$info = new PageInfo();
         parent::$info->setTitle("Trampoline Intercité | TITRE ICI");
-        parent::$info->setCss("singlenews/singlenews.css");
+        parent::$info->setCss("singlenews/singlenews.css", "singlenews/resulttable.css");
     }
 
     public static function GetTextHtml()
     {
-        return "<p>Paragraph no 1 text text</p><p>Paragraph no 2 text text</p>";
+        return "<p class=\"lato thin\">Paragraph no 1 text text</p><p class=\"lato thin\">Paragraph no 2 text text</p>";
     }
 
     public static function GetResultTable()
--- site/includes/views/singlenews.php
+++ site/includes/views/singlenews.php
@@ -1,9 +1,52 @@
-<h1>Doje is da new god</h1>
+<h1 class="lato medium">Doje is da new god</h1>
 <hr>
 
-<img src="/assets/news/<?php echo static::$img; ?>">
-
-<?php
+<div class="wrapper">
+    <div class="left">
+        <?php
 echo static::GetTextHtml();
 echo static::GetResultTable();
 ?>
+
+        <p class="lato thin">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptate vero minus dolores tenetur odio dolorum, maiores explicabo, consectetur hic deserunt quod
+            perspiciatis
+            <b>aliquid</b> provident qui itaque quas possimus nulla. Vel voluptate labore odio ea, nulla itaque deleniti porro tenetur esse obcaecati tempore excepturi accusantium consectetur fugiat!
+            Ducimus,
+            modi
+            provident?
+        </p>
+    </div>
+
+    <div class="right">
+        <img src="/assets/news/<?php echo static::$img; ?>">
+    </div>
+</div>
+
+<h4 class="lato bold">Titre du tableau!</h4>
+<div class="table-wrapper">
+    <table>
+        <tbody>
+            <tr>
+                <th class="lato bold">Athlète</th>
+                <th class="lato bold">Trampoline</th>
+                <th class="lato bold">Double-mini trampoline</th>
+                <th class="lato bold">Trampoline synchronisé</th>
+            </tr>
+            <tr>
+                <td colspan="5" class="lato medium">N5 whatever</td>
+            </tr>
+            <tr>
+                <td><a href="/teams/stephane" class="lato thin">Stephane</a></td>
+                <td class="lato light">Or</td>
+                <td class="lato light">5</td>
+                <td class="lato light">9</td>
+            </tr>
+            <tr>
+                <td><a href="/teams/michael" class="lato thin">Michael</a></td>
+                <td class="lato light">6</td>
+                <td></td>
+                <td class="lato light">9</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
